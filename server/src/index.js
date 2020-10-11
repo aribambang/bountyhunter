307dbed511ecd3bdf2c6fe19d04632d762e37dad
Ari Bambang Kurniawan <aribk210@gmail.com>
2020-10-11 15:32:28 +0700

    add tracks and mobile client

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -6,12 +7,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const authRoutes = require('./routes/authRoutes');
-const requireAuth = require('./middleware/authRequire');
+const trackRouter = require('./routes/trackRoutes');
+const requireAuth = require('./middleware/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRouter);
 
 const mongoUri = `mongodb+srv://admin:${process.env.PASSWD}@cluster0.wxp6g.gcp.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri, {
