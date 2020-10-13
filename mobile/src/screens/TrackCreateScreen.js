a6509718e2be45c4ef624c3a4274d5bc9feef03b
Ari Bambang Kurniawan <aribk210@gmail.com>
2020-10-13 23:52:09 +0700

    add map

--- mobile/src/screens/TrackCreateScreen.js
+++ mobile/src/screens/TrackCreateScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
+import { requestPermissionAsync } from 'expo-location';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
-      <Text style={{ fontSize: 48 }}>Account Screen</Text>
+      <Text style={{ fontSize: 48 }}>Create a Track</Text>
+      <Map />
     </SafeAreaView>
   );
 };
